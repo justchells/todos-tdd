d8283a1c607f6090d723a9cb6eaaea24132e501c
justchells <justchells@gmail.com>
2015-03-03 11:26:48 +0530

    First unit test and url mapping, dummy view

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tasklist.views.home', name='home'),
+    url(r'^$', 'todos.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
