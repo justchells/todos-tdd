ff4256a5939281782bb9f9cff07b5c0237cfdd8d
justchells <justchells@gmail.com>
2015-03-04 11:46:18 +0530

    New URL for adding items to an existing todo list

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'todos.views.home_page', name='home'),
     url(r'^todos/the-only-todo-list-in-the-world/$', 'todos.views.view_list', name='view_list'),
+    url(r'^todos/new$', 'todos.views.new_list', name='new_list')
     #url(r'^admin/', include(admin.site.urls)),
 )
