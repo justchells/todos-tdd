d4a571eea21f6e8de7b5f706b0b5399dc6739a20
justchells <justchells@gmail.com>
2015-03-05 00:00:20 +0530

    refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,6 +9,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.implicitly_wait(5)
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
@@ -60,6 +61,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc.
+        self.browser.refresh()
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
