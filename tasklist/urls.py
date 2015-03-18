b317bab83adcfcaf3a824294a1b6a8ffe2980dec
justchells <justchells@gmail.com>
2015-03-18 18:06:20 +0530

    Custom Persona auth backend + custom user model

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'todos.views.home_page', name='home'),
     url(r'^todos/', include('todos.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     #url(r'^admin/', include(admin.site.urls)),
 )
